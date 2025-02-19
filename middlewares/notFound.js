ab8dfae02906d8231e9b1048402a76b4ed8d5fb2
Kevin-Garri <kevin.alexander.garri29@gmail.com>
2025-02-19 12:18:17 +0100

    add: error, notfound

---
+++ middlewares/notFound.js
@@ -0,0 +1,10 @@
+const notFound = (req, res, next) => {
+  res.status(404);
+  res.json({
+    message: "risorsa non trovata",
+    status: 404,
+    error: "Not Found",
+  });
+};
+
+module.exports = notFound;
