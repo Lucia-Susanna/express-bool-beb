b484e643293d3e5191a2880497fa65c8c0986e55
SimoneZampieri <simonezampieri106@gmail.com>
2025-02-24 15:42:56 +0100

    feat: enhance index route with filtering options and improve image handling

--- controllers/bebController.js
+++ controllers/bebController.js
@@ -2,11 +2,56 @@ const connection = require("../data/db");
 
 // rotta index
 const index = (req, res) => {
-  const sql = "SELECT * FROM homes";
+  const { city, minRooms, maxRooms } = req.query;
 
-  connection.query(sql, (error, result) => {
-    if (error) return res.status(500).json({ error: err.message });
-    res.json(result);
+  let sql = `SELECT homes.*, ROUND(AVG(reviews.vote), 0) AS avg_vote, hosts.id AS host_id, hosts.name AS host_name, hosts.surname AS host_surname, hosts.email AS host_mail, hosts.phone AS host_phone
+  FROM homes
+  LEFT JOIN reviews ON homes.id = reviews.home_id
+  LEFT JOIN hosts ON homes.host_id = hosts.id`;
+
+  let conditions = [];
+  let values = [];
+
+  if (city) {
+    conditions.push("homes.address LIKE ?");
+    values.push(`%, ${city}`);
+  }
+
+  if (minRooms) {
+    conditions.push("homes.rooms >= ?");
+    values.push(minRooms);
+  }
+
+  if (maxRooms) {
+    conditions.push("homes.rooms <= ?");
+    values.push(maxRooms);
+  }
+
+  if (conditions.length > 0) {
+    sql += " WHERE " + conditions.join(" AND ");
+  }
+
+  sql += " GROUP BY homes.id";
+
+  const sqlImg = `SELECT home_id, url FROM images`;
+
+  connection.query(sql, values, (error, result) => {
+    if (error) return res.status(500).json({ error: error.message });
+
+    connection.query(sqlImg, (error, imgResult) => {
+      if (error) return res.status(500).json({ error: error.message });
+
+      const homes = result.map((home) => {
+        return {
+          ...home,
+          imgs: imgResult
+            .filter((img) => img.home_id === home.id)
+            .map((img) => img.url),
+        };
+      });
+
+      res.json(homes);
+    });
   });
 };
 
@@ -32,27 +77,20 @@ const show = (req, res) => {
     connection.query(sqlReviews, [id], (error, reviewResult) => {
       if (error) return res.status(500).json({ error: error.message });
       dbReviews.reviews = reviewResult;
-    });
 
-    connection.query(sqlImgs, [id], (error, imgResult) => {
-      if (error) return res.status(500).json({ error: error.message });
-      const imgData = imgResult.map((img) => ({
-        ...img,
-        url: `${req.imagePath}/${img.url}`,
-      }));
-      dbReviews.images = imgData.map((img) => img.url);
-      res.json(dbReviews);
+      connection.query(sqlImgs, [id], (error, imgResult) => {
+        if (error) return res.status(500).json({ error: error.message });
+        dbReviews.images = imgResult.map((img) => img.url);
+        res.json(dbReviews);
+      });
     });
   });
 };
 
 //updateLikes
-
 const updateLikes = (req, res) => {
   const id = req.params.id;
-  const sql = `UPDATE homes 
-SET likes = likes + 1 
-WHERE id = ?;`;
+  const sql = `UPDATE homes SET likes = likes + 1 WHERE id = ?`;
 
   connection.query(sql, [id], (error, results) => {
     if (error) return res.status(500).json({ error: error.message });
@@ -60,7 +98,7 @@ WHERE id = ?;`;
   });
 };
 
-//store
+//storeHomes
 const storeHomes = (req, res) => {
   const {
     host_id,
@@ -72,8 +110,7 @@ const storeHomes = (req, res) => {
     address,
   } = req.body;
 
-  const sql = `INSERT INTO homes ( host_id, description, rooms, beds, restrooms, square_meters, address, likes) VALUES
- (?, ?, ?, ?, ?, ?, ?, 0)`;
+  const sql = `INSERT INTO homes (host_id, description, rooms, beds, restrooms, square_meters, address, likes) VALUES (?, ?, ?, ?, ?, ?, ?, 0)`;
 
   connection.query(
     sql,
@@ -87,7 +124,7 @@ const storeHomes = (req, res) => {
   );
 };
 
-//store review
+//storeReview
 const storeReview = (req, res) => {
   const id = req.params.id;
 
